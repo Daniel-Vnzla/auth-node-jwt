ceff7691f9fc02f14285bb0e5e7f135554555b46
unknown <rune.escape888@gmail.com>
2020-10-29 12:37:36 -0400

    refact: user routes move to controllers

---
+++ controllers/user.js
@@ -0,0 +1,36 @@
+const User = require('../models/user.js');
+
+const { validateSingUpUserData, validateSingInUserData } = require('../utils/validation.js');
+const { createUser, findUserAuth } = require('../utils/mongoose.js');
+
+
+const singIn = async (req, res) => {
+	try {
+		await validateSingInUserData(req.body);
+		const user = await findUserAuth(req.body);
+		return res.json({ user });
+	}
+	catch(err){
+		console.log(err);
+		return res.status(500).json({ message: err.message })
+	}
+}
+
+const singUp = async (req, res) => {
+	const { username, email, password, confirmPassword } = req.body;
+
+	try{
+		await validateSingUpUserData(req.body);
+		const newUser = await createUser(req.body);
+		return res.json(newUser);
+	}
+	catch(err){
+		return res.status(500).json({ message: err.message})
+	}
+
+}
+
+module.exports = {
+	singIn,
+	singUp,
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,9 @@
 const router = require('express').Router();
-const User = require('../models/user.js');
 
-const { validateSingUpUserData, validateSingInUserData } = require('../utils/validation.js');
-const { createUser, findUserAuth } = require('../utils/mongoose.js');
+const { singIn, singUp } = require('../controllers/user.js');
 
-
-router.post('/singin', async (req, res) => {
-	try {
-		await validateSingInUserData(req.body);
-		const user = await findUserAuth(req.body);
-		return res.json({ user });
-	}
-	catch(err){
-		console.log(err);
-		return res.status(500).json({ message: err.message })
-	}
-})
-
-router.post('/singup', async (req, res) => {
-	const { username, email, password, confirmPassword } = req.body;
-
-	try{
-		await validateSingUpUserData(req.body);
-		const newUser = await createUser(req.body);
-		return res.json(newUser);
-	}
-	catch(err){
-		return res.status(500).json({ message: err.message})
-	}
-
-})
+router.post('/singin', singIn);
+router.post('/singup', singUp);
 
 
 module.exports = router;
