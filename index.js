065866324236b47a9c29f69773c83528f1ea100b
unknown <rune.escape888@gmail.com>
2020-10-29 10:00:35 -0400

    add: env envioroment

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const path = require('path')
+const mongoose = require('mongoose');
+const path = require('path');
+require('env').config();
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+mongoose.connect('')
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use(express.json());
