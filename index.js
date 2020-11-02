203b6988fcb8c5808c4e6961dcc4bfeff846c98d
unknown <rune.escape888@gmail.com>
2020-11-02 14:23:11 -0400

    add: singup ftch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv').config();
+const cookieParser = require('cookie-parser');
 
 const dataBase = require('./db.js');
 
@@ -10,8 +11,12 @@ const app = express();
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(require('./routes/auth.js'));
 
 app.get('/',(req, res) => res.sendFile('views/home.html', { root: __dirname }))
+
+
+
 app.listen(PORT, () => console.log('Server Ready'))
